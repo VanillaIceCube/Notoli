cc698a0e7c6ef91d2e6a9cbb6055070f01e280c5
IceCobe <jaalaba@gmail.com>
2025-04-28 17:46:39 -0700

    updated Tolis & Notos

--- backend/notes/admin.py
+++ backend/notes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Toli, Noto
 
-# Register your models here.
+
+admin.site.register(Toli)
+admin.site.register(Noto)
--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
 
-class Note(models.Model):
-    note = models.CharField(max_length=255)
-    description = models.TextField()
+# Toli: Todo List
+class Toli(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return self.name
+
+
+# Noto: Notes
+class Noto(models.Model):
+    note = models.CharField(max_length=255)
+    toli = models.ForeignKey(Toli, on_delete=models.CASCADE, related_name='notes')
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.note
