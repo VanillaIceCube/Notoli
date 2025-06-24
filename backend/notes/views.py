46364380c50e5c9b8fa216624117d7e8fd5e3042
Jude Andrew Alaba <jaalaba@gmail.com>
2025-06-23 21:21:25 -0700

    Filter notes by user and remove frontend owner hacks

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,18 +1,47 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
 from .models import Workspace, TodoList, Note
 from .serializers import WorkspaceSerializer, TodoListSerializer, NoteSerializer
 
 
 class WorkspaceViewSet(viewsets.ModelViewSet):
-    queryset = Workspace.objects.all()
+    permission_classes = [IsAuthenticated]
     serializer_class = WorkspaceSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Workspace.objects.filter(
+            Q(owner=user) | Q(created_by=user) | Q(collaborators=user)
+        ).distinct()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user, created_by=self.request.user)
 
 
 class TodoListViewSet(viewsets.ModelViewSet):
-    queryset = TodoList.objects.all()
+    permission_classes = [IsAuthenticated]
     serializer_class = TodoListSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return TodoList.objects.filter(
+            Q(owner=user) | Q(created_by=user) | Q(collaborators=user)
+        ).distinct()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user, created_by=self.request.user)
 
 
 class NoteViewSet(viewsets.ModelViewSet):
-    queryset = Note.objects.all()
+    permission_classes = [IsAuthenticated]
     serializer_class = NoteSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(
+            Q(owner=user) | Q(created_by=user) | Q(collaborators=user)
+        ).distinct()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user, created_by=self.request.user)
