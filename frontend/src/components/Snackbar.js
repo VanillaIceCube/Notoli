d93c518188bbeeef2b7468bfa5d7f9e7a587a379
IceCobe <jaalaba@gmail.com>
2025-07-28 18:21:48 -0700

    Snackbar operational refactor

--- frontend/src/components/Snackbar.js
+++ frontend/src/components/Snackbar.js
@@ -3,28 +3,29 @@ import {
   Alert
 } from '@mui/material';
 
-// Snackbar states
+export default function Login() {
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarSeverity, setSnackbarSeverity] = useState('success');
   const [snackbarMessage, setSnackbarMessage] = useState('');
 
-<Snackbar
-        open={snackbarOpen}
-        autoHideDuration={3000}
-        onClose={handleSnackbarClose}
-        anchorOrigin={{ vertical:'bottom', horizontal:'right' }}
-      >
-        <Alert sx={{ width:'100%' }}
-          onClose={handleSnackbarClose}
-          severity={snackbarSeverity}
-        >
-          {snackbarMessage}
-        </Alert>
-      </Snackbar>
-
-
-  // Snackbar function
   const handleSnackbarClose = (_event, reason) => {
     if (reason === 'clickaway') return;
     setSnackbarOpen(false);
-  };
+  }
+
+  return (
+    <Snackbar
+      open={snackbarOpen}
+      autoHideDuration={3000}
+      onClose={handleSnackbarClose}
+      anchorOrigin={{ vertical:'bottom', horizontal:'right' }}
+    >
+      <Alert sx={{ width:'100%' }}
+        onClose={handleSnackbarClose}
+        severity={snackbarSeverity}
+      >
+        {snackbarMessage}
+    </Alert>
+    </Snackbar>
+  );
+}
