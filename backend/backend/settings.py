2846a9556bec5b78a3cc9fbe545bd83ed5b62105
IceCobe <jaalaba@gmail.com>
2025-12-07 16:23:26 -0800

    added secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,10 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-syo-+2*nblyvgzolwlclzv%4v42wh%^tv&++4fvt^=!+(%puzd"
-
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ.get("SECRET_KEY", "default-key")
 DEBUG = os.environ.get("DEBUG", "1") == "1"
 ALLOWED_HOSTS = [
     "localhost",
