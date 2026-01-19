9043da653033775ce27c4288c697a4ce32004d8a
IceCobe <jaalaba@gmail.com>
2026-01-18 22:43:25 -0800

    more comments

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -3,4 +3,6 @@ from django.db import models
 
 
 class User(AbstractUser):
+    # We subclass AbstractUser (Django's default user shape) to add a unique email field.
+    # This custom User is used in place of auth.User via AUTH_USER_MODEL.
     email = models.EmailField(unique=True)
