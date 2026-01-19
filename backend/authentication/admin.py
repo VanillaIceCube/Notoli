06b897e0b83b67beb2d80de0dfbca1cd23cabfc5
IceCobe <jaalaba@gmail.com>
2026-01-18 22:39:47 -0800

    added very large comment

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import Group, Permission
 
 from .models import User
 
+# I'm removeing Group and Permission, cause I have another group called authentication,
+# and this group is called authentication and authorization, and I thoughts it would be
+# stupid to have both those groups. So then I had a bunch of code to move what was in
+# authentication and moved it to authentication and authorization, but that also felt
+# dumb. So my final solution was to just unregister it, cause I don't even use Group
+# in the first place.
 admin.site.register(User, DjangoUserAdmin)
 if admin.site.is_registered(Group):
     admin.site.unregister(Group)
