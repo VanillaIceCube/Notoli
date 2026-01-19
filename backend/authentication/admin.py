b56baebf9a20fcd944b8043f7f6949da5c53b7ca
IceCobe <jaalaba@gmail.com>
2026-01-18 22:27:27 -0800

    more minor changes

---
+++ backend/authentication/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.models import Group, Permission
+from .models import User
+
+
+admin.site.register(User, DjangoUserAdmin)
+if admin.site.is_registered(Group):
+    admin.site.unregister(Group)
+if admin.site.is_registered(Permission):
+    admin.site.unregister(Permission)
